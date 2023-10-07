368a7766afe6b5311cfae88c39799ef18fe77248
AndrewGalkin123 <127788127788a@gmail.com>
2023-10-07 19:30:09 +0300

    workind odd/even bids

--- src/components/Roulette.jsx
+++ src/components/Roulette.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { styles } from "/Casino/casino/src/styles"
-import Header from './Header';
 
-const Roulette = ({ chips, onChipsChange })  => {
+const Roulette = ({ chips, onChipsChange }) => {
 
     let nums = ["00", "0"];
     for (let i = 1; i <= 36; i++) {
@@ -19,52 +18,82 @@ const Roulette = ({ chips, onChipsChange })  => {
     const handleClick = () => {
         const numberInput = document.getElementById("number");
         const bidInput = document.getElementById("bid");
+        const evenRadio = document.getElementById("evenRadio");
+        const oddRadio = document.getElementById("oddRadio");
+        const even = document.getElementById("even");
+        const odd = document.getElementById("odd");
+
         if (numberInput.value > 36) {
             return alert("Такого числа нету");
-          }
-          if (numberInput.value !== "" && bidInput.value !== "") {
-            const bid = parseInt(bidInput.value, 10);
-
-            // Проверяем, не превышает ли ставка текущее количество фишек
-            if (bid <= chips) {
-                const result = generateRandom();
-                setRandomResult(result);
-      
-                // Вычитаем ставку из текущего количества фишек и обновляем состояние
-                const newChips = chips - bid;
-                onChipsChange(newChips); // Вызываем функцию обратного вызова для обновления фишек
-            } else {
-                alert("Недостаточно фишек для совершения ставки.");
-            }
         }
+        let oddNumber = 0;
+        let evenNumber = 0;
+        if (oddRadio.checked) {
+            oddNumber = parseInt(odd.value, 10);
+        } else if (evenRadio.checked) {
+            evenNumber = parseInt(even.value, 10);
+        }
+
+        const bid = parseInt(bidInput.value, 10) + oddNumber || evenNumber;
+        const number = parseInt(numberInput.value, 10);
+        console.log(bid)
+        if (bid > chips) {
+            alert("Недостаточно фишек")
+        }
+
+        // Проверяем, не превышает ли ставка текущее количество фишек
+        if (bid <= chips) {
+            const result = generateRandom();
+            setRandomResult(result);
+            let newChips = chips - bid;  // Вычитаем ставку из текущего количества фишек и обновляем состояние
+            if (result === number) {
+                newChips += 36 * bid
+            }
+            if (evenRadio.checked && result % 2 === 0) {
+                newChips += evenNumber * 2
+            } else if (oddRadio.checked && result % 2 !== 0) {
+                newChips += oddNumber * 2
+            }
+
+            onChipsChange(newChips); // Вызываем функцию обратного вызова для обновления фишек
+
+        }
+
 
     }
 
     return (
         <main style={styles.Roulette}>
-           
+
             {randomResult}
             <br></br>
             <p>Ваше число:</p>
-            <input id="number" type="number"></input><button>Подтвердить</button>
+            <input id="number" type="number"></input>
             <p>Ваша ставка:</p>
-            <input id="bid" type="number"></input><button onClick={handleClick}>Подтвердить</button>
+            <input id="bid" type="number"></input>
+            <br></br>
+            <br></br>
             <form>
-                <p>
-                    <input type="radio" value="even" name="number" />EVEN
-                </p>
-                <p>
-                    <input type="radio" value="odd" name="number" />ODD
-                </p>
+
+                <input type="radio" value="0" name="number" id="evenRadio" />EVEN
+
+                <input id="even"></input>
+                <br></br>
+                <br></br>
+                <input type="radio" value="0" name="number" id="oddRadio" />ODD
+
+                <input id="odd"></input>
             </form>
             <form>
                 <p>
-                    <input type="radio" value="even" name="number" />1-18
+                    <input type="radio" value="even" name="number1" />1-18
                 </p>
                 <p>
-                    <input type="radio" value="odd" name="number" />19-36
+                    <input type="radio" value="odd" name="number1" />19-36
                 </p>
             </form>
+            <br></br>
+            <button onClick={handleClick}>Подтвердить</button>
         </main>
     )
 }
